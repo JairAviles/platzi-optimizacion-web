84734ea6243b74130f693bb30748071c18607c0f
Jair Aviles <jair.aviles@segment.com>
2022-10-13 19:07:14 -0700

    Code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@ const webpack = require('webpack')
 const path = require('path')
 
 const nodeEnv = process.env.NODE_ENV || 'development'
+const shouldAnalyze = process.argv.includes('--analyze')
+const plugins = []
+
+if (shouldAnalyze) {
+  const { BundleAnalyzerPlugin } = module.require('webpack-bundle-analyzer')
+  plugins.push(new BundleAnalyzerPlugin())
+}
 
 const config = {
   mode: nodeEnv,
@@ -9,10 +16,16 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist',
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
   },
   devServer: {
     contentBase: '.',
+  },
+  plugins,
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
   },
 }
 
