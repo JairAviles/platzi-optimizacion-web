a3a9c50e3541920e6e9e6d4f7517b96eaafc38d7
Jair Aviles <jair.aviles@segment.com>
2023-05-26 17:53:56 -0700

    modal video lazy load

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,11 +1,15 @@
-import  { openModal } from './open-modal'
+// Lazy loading - webpack
 
 export const modalListener = (event) => {
   event.preventDefault()
   const img = event.target
   const link = img.parentElement
+  const videoId = link.dataset.videoid
 
-  if (link && link.classList.contains('js-video-link')){
-    openModal(link.dataset.videoid)
+  if (link && link.classList.contains('js-video-link')) {
+    // Lazy loading
+    import(/* webpackChunkName: "modal" */ './open-modal').then(({ openModal }) => {
+      openModal(videoId)
+    })
   }
 }
